aa955f92720acd8e6fc4695a692dd42ebc86523a
Kari Wassholm <kari.wassholm@gmail.com>
2021-12-03 19:02:57 +0200

    add day 1

---
+++ 1/a.ex
@@ -0,0 +1,12 @@
+contents =
+  File.stream!("./input.txt")
+  |> Enum.map(&String.to_integer(String.trim(&1)))
+  |> Enum.map_reduce(0, fn x, prev -> {x > prev, x} end)
+  |> (fn {x, _} -> x end).()
+  |> Enum.count(&(&1 == true))
+
+# |> Enum.chunk_every(2, 1, :discard)
+# |> Enum.count(fn [a, b] -> b > a end)
+# IO.inspect(contents, limit: :infinity)
+
+IO.inspect(contents - 1, limit: :infinity)
